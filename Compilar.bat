acd4bb4e7f64d81708fd8c30c958bb64cd326f0f
PauloThiagoMS <paulinho_marreiros@hotmail.com>
2017-05-16 00:58:58 -0400

    Update Compilar.bat

--- Compilar.bat
+++ Compilar.bat
@@ -1,5 +1,19 @@
 @echo off
 color b1
-Title KNN in Lisp
+Title Knn_in_Lisp
+del /q /f KNN.fas
 clisp -c KNN.lisp
-start KNN.fas
+echo.
+echo Fim da compilacao!
+pause
+cls
+echo Defina os seguintes parametro:
+echo :maxsim :maxmem :core :clock :ram :display :pixel :K
+echo .
+echo .
+echo Antes de fechar, salve o arquivo!
+start /wait user.txt
+cls
+KNN.fas > out.txt
+echo Resultados:
+out.txt
